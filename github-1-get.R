d15e042da9e28213e73a4adf2c01f6ecfd021534
hadley <h.wickham@gmail.com>
2015-09-25 10:40:25 -0500

    Updates for dplyr and github

--- github-1-get.R
+++ github-1-get.R
@@ -4,16 +4,15 @@ library(dplyr)
 
 # Github API -------------------------------------------------------------------
 
-user <- Sys.getenv("GITHUB_USER")
-pwd <- Sys.getenv("GITHUB_PASS")
-if (user == "" || pwd == "") {
-  stop("Set GITHUB_USER and GITHUB_PASS env vars", call. = FALSE)
+pat <- Sys.getenv("GITHUB_PAT")
+if (pat == "") {
+  stop("Set GITHUB_PAT env vars", call. = FALSE)
 }
 
 # install_github("rgithub", "cscheid")
 base <- "https://api.github.com"
 config <- c(
-  authenticate(user, pwd, type = "basic"), 
+  authenticate(pat, "", type = "basic"), 
   add_headers(
     Accept = "application/vnd.github.preview", 
     "User-Agent" = "hadley/r-python"
@@ -26,7 +25,7 @@ rate_limit <- function() {
 repos <- function(query) {
   Sys.sleep(60 / 20) # 20 requests per minute
   path <- paste0(base, "/search/repositories")
-  qs <- list(q = paste("language:r", query), per_page = 1)
+  qs <- list(q = query, per_page = 1)
   
   req <- GET(path, config, query = qs)
   stop_for_status(req)
@@ -38,22 +37,36 @@ cur_year <- year(today())
 
 past <- expand.grid(year = 2011:(cur_year - 1), month = 1:12)
 pres <- data.frame(year = cur_year, month = 1:(month(today()) - 2))
-months <- rbind(past, pres)
+months <- bind_rows(past, pres)
 
-all <- rbind(cbind(months, lang = "r"), cbind(months, lang = "python"))
-all <- all %.% group_by(lang) %.% arrange(year, month)
-all <- all %.% mutate(
-  year_next = lead(year, default = cur_year), 
-  month_next = lead(month, default = month(today()) - 1)
-)
+all <- bind_rows(
+  months %>% mutate(lang = "r"),
+  months %>% mutate(lang = "python")
+) 
 
-all$query <- paste0("language:", all$lang, 
-  " created:", all$year, "-", all$month, "..", 
-  all$year_next, "-", all$month_next)
-all$count <- NA
+last_month <- month(today()) - 1L
+all <- all %>% 
+  group_by(lang) %>% 
+  arrange(year, month) %>%
+  mutate(
+    year_next = lead(year, default = cur_year), 
+    month_next = lead(month, default = last_month),
+    query = paste0("language:", lang,  " created:", year, "-", month, "..", 
+      year_next, "-", month_next),
+    count = NA_integer_
+  )
+
+if (file.exists("github.rds")) {
+  cached <- readRDS("github.rds") %>% ungroup() %>% mutate(lang = as.character(lang))
+  
+  all <- bind_rows(
+    cached,
+    all %>% anti_join(cached, c("year", "month", "lang"))
+  )
+}
 
 missing <- seq_along(all$count)[is.na(all$count)]
-for(i in missing) {
+for (i in missing) {
   all$count[[i]] <- repos(all$query[i])
   cat(".")
 }
