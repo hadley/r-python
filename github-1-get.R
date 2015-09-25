f0d598dc0f032352a15f73d3afe5c55f34392c0f
hadley <h.wickham@gmail.com>
2015-09-25 10:50:48 -0500

    Final github data tweaks

--- github-1-get.R
+++ github-1-get.R
@@ -23,7 +23,7 @@ rate_limit <- function() {
 }
 
 repos <- function(query) {
-  Sys.sleep(60 / 20) # 20 requests per minute
+  Sys.sleep(60 / 30) # 30 requests per minute
   path <- paste0(base, "/search/repositories")
   qs <- list(q = query, per_page = 1)
   
@@ -37,37 +37,34 @@ cur_year <- year(today())
 
 past <- expand.grid(year = 2011:(cur_year - 1), month = 1:12)
 pres <- data.frame(year = cur_year, month = 1:(month(today()) - 2))
-months <- bind_rows(past, pres)
+months <- bind_rows(past, pres) %>% 
+  arrange(year, month) %>% 
+  mutate(
+    start = as.Date(ISOdate(year, month, 1)),
+    end = start + months(1) - days(1)
+  )
 
 all <- bind_rows(
-  months %>% mutate(lang = "r"),
-  months %>% mutate(lang = "python")
-) 
-
-last_month <- month(today()) - 1L
-all <- all %>% 
-  group_by(lang) %>% 
-  arrange(year, month) %>%
+    months %>% mutate(lang = "r"),
+    months %>% mutate(lang = "python")
+  ) %>% 
   mutate(
-    year_next = lead(year, default = cur_year), 
-    month_next = lead(month, default = last_month),
-    query = paste0("language:", lang,  " created:", year, "-", month, "..", 
-      year_next, "-", month_next),
+    query = paste0("language:", lang,  " created:", start, "..", end),
     count = NA_integer_
   )
-
-if (file.exists("github.rds")) {
-  cached <- readRDS("github.rds") %>% ungroup() %>% mutate(lang = as.character(lang))
-  
-  all <- bind_rows(
-    cached,
-    all %>% anti_join(cached, c("year", "month", "lang"))
-  )
-}
+# 
+# if (file.exists("github.rds")) {
+#   cached <- readRDS("github.rds") %>% ungroup() %>% mutate(lang = as.character(lang))
+#   
+#   all <- bind_rows(
+#     cached,
+#     all %>% anti_join(cached, c("year", "month", "lang"))
+#   )
+# }
 
 missing <- seq_along(all$count)[is.na(all$count)]
 for (i in missing) {
-  all$count[[i]] <- repos(all$query[i])
   cat(".")
+  all$count[[i]] <- repos(all$query[i])
 }
 saveRDS(all, "github.rds")
